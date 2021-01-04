71b397b2678992566b6dd409208c6037fb5d8044
hakmad <39906682+hakmad@users.noreply.github.com>
2021-01-04 13:37:12 +0100

    Added basic ROT13 and ROT47 encryption functions

---
+++ encrypt.py
@@ -0,0 +1,30 @@
+def rot13(text, shift):
+    new_text = ""
+
+    for letter in text:
+        if letter.isalpha():
+            if letter.isupper():
+                new_letter = chr((ord(letter) - ord("A") + shift) % 26
+                    + ord("A"))
+            elif letter.islower():
+                new_letter = chr((ord(letter) - ord("a") + shift) % 26
+                    + ord("a"))
+        else:
+            new_letter = letter
+
+        new_text += new_letter
+
+    return new_text
+
+def rot47(text, shift):
+    new_text = ""
+
+    for letter in text:
+        if letter.isascii() and not(letter.isspace()):
+            new_letter = chr((ord(letter) - ord("!") + shift) % 94 + ord("!"))
+        else:
+            new_letter = letter
+
+        new_text += new_letter
+
+    return new_text
